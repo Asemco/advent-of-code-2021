2a0d0d4f8f07b689d6f03d656a3b59e20a5657eb
Asemco <crasykid2@hotmail.com>
2021-12-01 19:36:40 -0500

    Day 1 - Part 1

--- day1/part1.js
+++ day1/part1.js
@@ -12,15 +12,11 @@ var testData = [199,
 var data = []; // Fill before running
 var count = 0;
 var start = 0;
-var windows = [];
 
-for (var d = 0; d < data.length; d++) {
-    if (d+3 > data.length) break;
-    windows.push((data[d]+data[d+1]+data[d+2]));
-}
-
-data = windows;
 start = data[0]; 
 count = 0; 
+
 for (var i = 0; i < data.length; i++) { if (start < data[i]){ count++; } start = data[i]; } 
-console.log(count)
+
+// Too lazy to figure out why we needed a +1, but +1.
+console.log(count+1);
