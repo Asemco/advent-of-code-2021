9d18ca0967546b6bc3edb3b3f6721921b0f493d9
Asemco (Kevin Taylor) <crasykid2@hotmail.com>
2021-12-02 20:06:05 -0500

    Day 2 Complete.  This one was pretty E Z

---
+++ day2/part1.js
@@ -0,0 +1,33 @@
+/* Test Code */
+
+var tdata = ["forward 5",
+"down 5",
+"forward 8",
+"up 3",
+"down 8",
+"forward 2"
+];
+
+var tdataUsable = tdata.map( (d) => { return d.split(" ")});
+
+var x = 0; var y = 0; 
+tdataUsable.forEach( (d) => { 
+    if (d[0] == "forward") x+=parseInt(d[1]); 
+    if (d[0] == "up") y-=parseInt(d[1]); 
+    if (d[0] == "down") y+=parseInt(d[1]); 
+}); 
+console.log ("x: " + x + " * y: " + y + " = " + x * y); // Answer
+
+/* Real Code */
+
+var data = []; // Fill before running
+
+var dataUsable = data.map( (d) => { return d.split(" ")});
+
+var x = 0; var y = 0; 
+dataUsable.forEach( (d) => { 
+    if (d[0] == "forward") x+=parseInt(d[1]); 
+    if (d[0] == "up") y-=parseInt(d[1]); 
+    if (d[0] == "down") y+=parseInt(d[1]); 
+}); 
+console.log ("x: " + x + " * y: " + y + " = " + x * y); // Answer
